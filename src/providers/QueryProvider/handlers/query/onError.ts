8cdf315ca177d820d79fbb2e81c371a7e37afec4
Túlio Kenji <162953537+TulioKenji@users.noreply.github.com>
2026-04-10 21:43:34 -0300

    Revert "feat: instalacao de bibliotecas e implementacao de providers"

--- src/providers/QueryProvider/handlers/query/onError.ts
+++
@@ -1,8 +0,0 @@
-import { toast } from "react-hot-toast";
-
-export function queryErrorHandler(error: Error) {
-
-
-  // Fallback para outros erros genéricos
-  toast.error(error.message || 'Ocorreu um erro inesperado. Tente novamente Mais tarde.');
-}
