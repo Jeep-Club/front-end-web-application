0260459a9e3e0b1cfdd638b39ed19748f08e6fb0
joaoapassos <joaoapassjp@gmail.com>
2026-05-09 21:54:37 -0300

    fix: corrigir documentação da função getCurrentDateAndTime

--- src/utils/several/getCurrentDateAndTime.ts
+++ src/utils/several/getCurrentDateAndTime.ts
@@ -1,5 +1,20 @@
 
-
+/**
+ * Função para obter a data e hora atual formatada em português do Brasil.
+ * @description Esta função retorna um objeto contendo a data e hora atual formatada, incluindo o nome dos dias da semana em português do Brasil, além de informações detalhadas sobre o dia, mês, ano, hora, minuto e segundo.
+ * @returns {Object} Objeto contendo a data e hora atual formatada, incluindo:
+ *   - `date`: Data formatada em português do Brasil
+ *   - `time`: Hora formatada em português do Brasil
+ *   - `daysOfWeek`: Array com os nomes dos dias da semana em português do Brasil
+ *   - `day`: Dia do mês (com dois dígitos)
+ *   - `dayOfWeek`: Nome do dia da semana em português do Brasil
+ *   - `month`: Mês (com dois dígitos)
+ *   - `year`: Ano
+ *   - `hour`: Hora (com dois dígitos)
+ *   - `hourNumber`: Número da hora
+ *   - `minute`: Minuto (com dois dígitos)
+ *   - `second`: Segundo (com dois dígitos)
+ */
 export const getCurrentDateAndTime = ()=>{
     const d = new Date();
 
@@ -18,6 +33,6 @@ export const getCurrentDateAndTime = ()=>{
         hour: d.getHours().toString().padStart(2, '0'),
         hourNumber: d.getHours(),
         minute: d.getMinutes().toString().padStart(2, '0'),
-        secude: d.getSeconds().toString().padStart(2, '0'),
+        second: d.getSeconds().toString().padStart(2, '0'),
     })
 }
