30a4cc19d7f02e8227677828ef728cffd6e9545a
Vitor Menezes <161902126+Vitorram@users.noreply.github.com>
2026-05-13 17:41:25 +0000

    fix: ajustar estilo da mensagem de erro no formulário de registro

--- src/components/pages/register/Forms.tsx
+++ src/components/pages/register/Forms.tsx
@@ -192,7 +192,7 @@ export function RegisterForm() {
                     />
 
                     {serverError && (
-                        <p className="text-[var(--fs-xs)] text-[var(--danger)] bg-[var(--red-100)]/10 border border-[var(--danger)] rounded-lg px-3 py-2">
+                        <p className=" text-[var(--fs-md)] text-[var(--danger)] bg-[var(--red-100)] rounded-lg px-3 py-2">
                             {serverError}
                         </p>
                     )}
